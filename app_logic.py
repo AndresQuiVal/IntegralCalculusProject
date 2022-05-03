4cb72ecf185d67ef008f634f664e8edc9b48a59a
AndresQuiVal <quirozval.andres@hotmaill.com>
2022-05-03 18:54:55 -0500

    Created area calculation of the rectangles

--- app_logic.py
+++ app_logic.py
@@ -1,4 +1,77 @@
 """
 Here goes all the math functions for solving integrals;
 later on will be exported to main.py
+
+NOTES:
+
+base of rectangles:
+base = (last_limit - first_limit) / number_rectangles
+
+Height of rectangle:
+f(x [left | right | middle]) 
+
+Area of rectangle:
+area_recangle = f(x [left | right | middle]) *  base
+
+Area under curve:
+integral = area_rectangle * number_rectangles
+
+OUTPUT 'integral'
 """
+
+def f(x : int, function : str):
+    function.replace("x", str(x))
+    return eval(function)
+
+
+def get_left_rectangle(function, rectangles, first_limit, last_limit):
+    if not function or not rectangles:
+        return
+    
+    area_rectangles = 0
+    base = (last_limit - first_limit) // rectangles
+
+    if base <= 0:
+        return -1 # invalid input, by now...
+
+    for i in range(first_limit, last_limit, base):
+        height = f(i, function)
+        area_rectangles += base * height
+    
+    return area_rectangles
+ 
+
+def get_right_rectangle(function, rectangles, first_limit, last_limit):
+    if not function or not rectangles:
+        return
+    
+    area_rectangles = 0
+    base = (last_limit - first_limit) // rectangles
+    
+    if base <= 0:
+        return -1 # invalid input, by now...
+
+    for i in range(first_limit, last_limit, base):
+        height = f(i + base, function)
+        area_rectangles += base * height
+    
+    return area_rectangles
+
+def get_middle_rectangle(function, rectangles, first_limit, last_limit):
+    if not function or not rectangles:
+        return
+    
+    area_rectangles = 0
+    base = (last_limit - first_limit) // rectangles
+
+    if base <= 0:
+        return -1 # invalid input, by now...
+
+    for i in range(first_limit + base, last_limit + 1, base):
+        height = f((i + (i + base) / 2), function)
+        area_rectangles += base * height
+    
+    return area_rectangles
+
+def get_trapezoid_rectangle(function, rectangles, first_limit, last_limit):
+    pass
--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 Main entrance of app and UI logic
 """
 
-import tkinter as tk
-from general import *
+from dataclasses import dataclass
 import components as cp
+from app_logic import *
+from general import *
+import tkinter as tk
+
 
 
 root = tk.Tk()
@@ -32,27 +35,58 @@ title.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 title = cp.AppLabel(main_area, text="By: \nJJ & Andrés Quiroz", background=MAIN_WINDOW_COLOR)
 title.pack(pady=20)
 
+
+# String vars
+
+function = tk.StringVar()
+rectangles_number = tk.StringVar()
+
 # grid control container
 
-main_entry = tk.Entry(main_area)
+main_entry = tk.Entry(main_area, textvariable=function)
+main_entry.insert(0, 'function expression')
 main_entry.pack(pady=20)
+
+
+rectangles_title = cp.AppLabel(main_area, text="Number of rectangles:", background=MAIN_WINDOW_COLOR)
+rectangles_title.pack()
+
+rectangles = tk.Scale(main_area, orient=tk.HORIZONTAL, variable=rectangles_number, from_=0, to=1000, background=MAIN_WINDOW_COLOR, bd=0)
+rectangles.pack(pady=20)
+
 
 main_grid = tk.Frame(main_area, width=500, height=500, background=MAIN_WINDOW_COLOR)
 main_grid.pack()
 
 # 4 buttons for 4 options
 
-tk.Button(main_grid, text="Left rectangle", width=15, height=2, command=lambda:solve_equation("left")).grid(row=0, column=0, pady=5, padx=5)
-tk.Button(main_grid, text="Right rectangle", width=15, height=2, command=lambda:solve_equation("right")).grid(row=0, column=1, pady=5, padx=5)
-tk.Button(main_grid, text="Midpoint rectangle", width=15, height=2, command=lambda:solve_equation("middle")).grid(row=1, column=0, pady=5, padx=5)
-tk.Button(main_grid, text="Trapezoid", width=15, height=2, command=lambda:solve_equation("trapezoid")).grid(row=1, column=1, pady=5, padx=5)
+tk.Button(main_grid, text="Left rectangle", width=15, height=2, command=lambda:get_integral("left")).grid(row=0, column=0, pady=5, padx=5)
+tk.Button(main_grid, text="Right rectangle", width=15, height=2, command=lambda:get_integral("right")).grid(row=0, column=1, pady=5, padx=5)
+tk.Button(main_grid, text="Midpoint rectangle", width=15, height=2, command=lambda:get_integral("middle")).grid(row=1, column=0, pady=5, padx=5)
+tk.Button(main_grid, text="Trapezoid", width=15, height=2, command=lambda:get_integral("trapezoid")).grid(row=1, column=1, pady=5, padx=5)
 
 
-def solve_equation(method : str):
+def get_integral(method : str):
     """
     Main function to solve the equation
     """
-    pass
+    global rectangles_number, function
+
+    rectangles_number_int = int(rectangles_number.get())
+    function_str = function.get()
+    
+    if method == "left":
+        res = get_left_rectangle(function_str, rectangles_number_int, 1, 500)
+    elif method == "right":
+        res = get_right_rectangle(function_str, rectangles_number_int, 1, 500)
+    elif method == "middle":
+        res = get_middle_rectangle(function_str, rectangles_number_int, 1, 500)
+    elif method == "trapezoid":
+        res = get_trapezoid_rectangle(function_str, rectangles_number_int, 1, 500)
+
+    import pdb; pdb.set_trace()
+
+    # render data...
 
 def main():
     window_esthetic()
